921e1581811a1944e7d346875d2138ca35f7bc7d
Matvey Bouhartsev <programmer@bouhartsev.top>
2021-11-08 16:41:03 +0300

    Camera frontend completed

--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -1,7 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setData = function(key, value) {
-        console.log(key,value);
-        // is not exists вывести в h1 дополнительно
-        // offline (is disconnected - разорвано соединение - или is not exists - не существует), connected, preview, onair
+document.addEventListener("DOMContentLoaded", function () {
+    let addition = document.querySelector("h1>span");
+    let description = document.querySelector("h2");
+
+    // add from url to html
+    let number = location.pathname.split('/');
+    number = number[number.length-1];
+    if (parseInt(number)) number = parseInt(number);
+    addition.innerText = number;
+
+    setData = function (key, value) {
+        console.log(key, value);
+        switch (value) {
+            case 'preview':
+            case 'onair':
+                document.body.classList.add(value);
+                description.innerText = '';
+                break;
+            default: // connected
+                document.body.classList.remove("preview");
+                document.body.classList.remove("onair");
+                description.innerText = value;
+                break;
+        }
     }
+    ws.onclose = function(event){
+        if (event.reason=="not exist") addition.classList.add("not-exist");
+        else addition.classList.add("disconnected");
+        setData("status", "offline");
+    }
+    ws.onopen = function(event) {
+        addition.classList.remove("not-exist");
+        addition.classList.remove("disconnected");
+        setData("status", "connected");
+    }
+
+    // offline (is disconnected - разорвано соединение - или doesn't exist - не существует), connected, preview, onair
 });
